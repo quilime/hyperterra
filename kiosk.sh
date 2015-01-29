a49223aa870e1c789b227f773225c6ae04dc2ed8
Gabriel Dunne <gdunne@quilime.com>
2015-01-29 15:53:18 -0800

    Updating run scripts

---
+++ kiosk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this file assumes it is in /opt/kiosk.sh
+# runs application with screensaver and screen-blanking disabled
+
+xset -dpms
+xset s off
+/home/ubuntu/hyperterra/linux/release/bin/LandscapeApp
+
+
